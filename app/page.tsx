3b476ebb7df8c6a25327262b5e5d2f5e82a67f53
dzyoo34 <165839007+dzyoo34@users.noreply.github.com>
2025-09-03 14:40:30 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,26 @@
-import { Hero } from "@/components";
+import { CustomFilter, SearchBar } from "@/components";
+import  Hero from "@/components/Hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
       <main className="overflow-hidden">
         <Hero />
-        </main>
+        <div className="mt-12 padding-x
+        padding-y max-width" id="discover">
+          <div className="home__text-container">
+            <h1 className="text-4xl
+            font-extrabold">Car Catalogue</h1>
+            <p>Exlpore the cars you might like</p>
+            <div className="home__filters">
+              <SearchBar />
+              <div className="home__filter-container">
+                <CustomFilter title="fuel" />
+                <CustomFilter title="year" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </main>    
   );
 }
