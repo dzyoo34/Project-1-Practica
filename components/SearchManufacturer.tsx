cbe0fdbed4e1d70c0758e8153b5ef8e61bb91d6c
dzyoo34 <165839007+dzyoo34@users.noreply.github.com>
2025-09-03 20:01:10 +0300

    Update SearchManufacturer.tsx

--- components/SearchManufacturer.tsx
+++ components/SearchManufacturer.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Image from 'next/image';
 import {useState, Fragment} from 'react';
-import { Combobox, Transition } from '@headlessui/react'
+import { Combobox, ComboboxOption, Transition } from '@headlessui/react'
 import { manufacturers } from '@/constants';
 import { SearchManufacturerProps } from '@/types'
 
@@ -18,7 +18,7 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
   )))
   return (
     <div className="search-manufacturer">
-      <Combobox>
+      <Combobox value={manufacturer} onChange={setManufacturer}>
         <div className="relative w-full">
         <Combobox.Button className="absolute top-[14px]">
           <Image
@@ -43,7 +43,41 @@ const SearchManufacturer = ({manufacturer, setManufacturer}: SearchManufacturerP
             afterLeave={() => setQuery('')}
             >
               <Combobox.Options>
-
+             {filteredManifactures.map((item) => (
+                <Combobox.Option
+                key={item}
+                className={({active}) => `
+                relative search-manufacturer__option
+                ${active ? 'bg-blue-700 text-white' : 'text-gray-900'}
+                `}
+                value={item}
+                >
+                {({selected, active}) => (
+                  <>
+                  <span 
+                  className={`block truncate ${
+                    selected ? 'font-medium' :
+                     'font-normal'
+                  }`}
+                  >
+                  {item}
+                  </span>
+                  {selected ? (
+                    <span 
+                    className={`absolute
+                      inset-y-0 left-0 flex
+                      items-center pl-3 ${
+                        active ? 'text-white' :
+                        'text-teal-600'
+                      }`}
+                  >
+                    </span>
+                  ) : null}
+                  </>
+             )}
+                </Combobox.Option>
+              )
+              )}
               </Combobox.Options>
           </Transition>
           </div>
