1346721c14c6296d758f6e10d15d33146f44461c
dzyoo34 <165839007+dzyoo34@users.noreply.github.com>
2025-09-03 19:59:28 +0300

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,14 @@
-import { CustomFilter, SearchBar } from "@/components";
+import { CarCard, CustomFilter, SearchBar } from "@/components";
 import  Hero from "@/components/Hero";
 import Image from "next/image";
+import {fetchCars} from '@/utils';
 
-export default function Home() {
+export default async function Home() {
+  const allCars = await fetchCars();
+
+
+  const isDataEmpty = !Array.isArray(allCars) || allCars.length < 1 || !allCars;
+
   return (
       <main className="overflow-hidden">
         <Hero />
@@ -12,14 +18,27 @@ export default function Home() {
             <h1 className="text-4xl
             font-extrabold">Car Catalogue</h1>
             <p>Exlpore the cars you might like</p>
+            </div>
             <div className="home__filters">
               <SearchBar />
               <div className="home__filter-container">
-                <CustomFilter title="fuel" />
-                <CustomFilter title="year" />
-              </div>
             </div>
           </div>
+
+{!isDataEmpty ? (
+<section>
+  <div className="home__cars-wrapper">
+    {allCars?.map((car, index) => (
+  <CarCard key={car.id || index} car={car} />
+))}
+  </div>
+</section>
+): (
+  <div className="home__error-container">
+    <h2 className="text-black text-xl font-bold">No results</h2>
+    <p>{allCars?.message}</p>
+  </div>
+)}
         </div>
       </main>    
   );
