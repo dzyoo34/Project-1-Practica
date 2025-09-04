2263bd7bb25dd9dd254d20922c4ba10d38632be5
dzyoo34 <165839007+dzyoo34@users.noreply.github.com>
2025-09-04 14:12:12 +0300

    Update index.ts

--- components/index.ts
+++ components/index.ts
@@ -6,7 +6,8 @@ import SearchBar from "./SearchBar";
 import CustomFilter from "./CustomFilter";
 import SearchManufacturer from "./SearchManufacturer";
 import CarCard from "./CarCard";
-
+import CarDetails from "./CarDetails";
+import CarProps from "./CarProps";
 export { 
     Hero,
     CustomButton,
@@ -16,4 +17,6 @@ export {
     CustomFilter,
     SearchManufacturer,
     CarCard,
+    CarDetails,
+     CarProps,
 };
