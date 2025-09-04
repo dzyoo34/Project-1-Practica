9b029c26ba26c9a90e369dd52e076c3c33d83bc0
dzyoo34 <165839007+dzyoo34@users.noreply.github.com>
2025-09-04 14:11:59 +0300

    Update Hero.tsx

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -15,15 +15,15 @@ const Hero = () => {
     <section className="hero">
       <div className="flex-1 pt-36 padding-x">
         <h1 className="hero__title">
-          Find, book, rent a car—quick and super easy!
+          Найди, закажи, арендуй машину быстро и легко!
         </h1>
 
         <p className="hero__subtitle mt-5">
-          Streamline your car rental experience with our effortless booking process.
+          Оптимизируйте процесс аренды автомобиля с помощью нашего простого процесса бронирования.
         </p>
 
         <CustomButton
-          title="Explore Cars"
+          title="Найти машины"
           containerStyles="bg-primary-blue text-white rounded-full mt-10"
           handleClick={handleScroll}
         />
@@ -35,7 +35,7 @@ const Hero = () => {
   
         <div className="hero__image relative w-[500px] h-[300px]">
           <Image
-            src="/hero.png"
+            src="/12.png"
             alt="hero"
             width={500}
             height={300}
